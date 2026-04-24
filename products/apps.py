6cfed38babe1fe47a5f5a47969d35554564369e6
unknown <amarathossain@gmail.com>
2026-04-24 15:51:33 +0600

    Server running successfully - Fixed apps.py error

--- products/apps.py
+++ products/apps.py
@@ -1,10 +1,5 @@
-from django.contrib import admin
-from .models import Product, Division, Zone, Area, Route, Outlet
+from django.apps import AppConfig
 
-# সব মডেল অ্যাডমিন প্যানেলে রেজিস্টার করা হচ্ছে
-admin.site.register(Product)
-admin.site.register(Division)
-admin.site.register(Zone)
-admin.site.register(Area)
-admin.site.register(Route)
-admin.site.register(Outlet)
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'products'
